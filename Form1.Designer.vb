af31dee50037061743376b7203f18fbc25e0aba0
Neitirite <83120083+Neitirite@users.noreply.github.com>
2024-05-12 18:19:35 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ProgressBar1 = New ProgressBar()
+        Label2 = New Label()
+        Label3 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label1.Location = New Point(12, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(506, 45)
+        Label1.TabIndex = 0
+        Label1.Text = "Тест на тему ""Прикладное ПО"""
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        ProgressBar1.Location = New Point(1291, 20)
+        ProgressBar1.Maximum = 10
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(459, 28)
+        ProgressBar1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Label2.Location = New Point(1291, 51)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(125, 21)
+        Label2.TabIndex = 2
+        Label2.Text = "Вопрос 1 из 10"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(204))
+        Label3.Location = New Point(12, 75)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(230, 45)
+        Label3.TabIndex = 3
+        Label3.Text = "Текст вопроса"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.Anchor = AnchorStyles.Left
+        RadioButton1.AutoSize = True
+        RadioButton1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(204))
+        RadioButton1.Location = New Point(24, 142)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(83, 25)
+        RadioButton1.TabIndex = 4
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Ответ 1"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.Anchor = AnchorStyles.Left
+        RadioButton2.AutoSize = True
+        RadioButton2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(204))
+        RadioButton2.Location = New Point(24, 191)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(83, 25)
+        RadioButton2.TabIndex = 4
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Ответ 2"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.Anchor = AnchorStyles.Left
+        RadioButton3.AutoSize = True
+        RadioButton3.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(204))
+        RadioButton3.Location = New Point(24, 239)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(83, 25)
+        RadioButton3.TabIndex = 4
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Ответ 3"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.Anchor = AnchorStyles.Left
+        RadioButton4.AutoSize = True
+        RadioButton4.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(204))
+        RadioButton4.Location = New Point(24, 288)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(83, 25)
+        RadioButton4.TabIndex = 4
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Ответ 4"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Button1.BackColor = Color.Teal
+        Button1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Button1.ForeColor = SystemColors.ButtonFace
+        Button1.Location = New Point(24, 334)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(139, 44)
+        Button1.TabIndex = 5
+        Button1.Text = "Проверить"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1755, 397)
+        Controls.Add(Button1)
+        Controls.Add(RadioButton4)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(ProgressBar1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Public questions(10) As String ' Массив с вопросами.
+    Public questions_count As Integer = 10 ' Количество вопросов, менять только в случае, если в массиве присутствует достаточное количество вопросов.
+    Public answers(40) As String ' Доступные ответы - количество вопросов * 4 (т.к. у каждого вопроса 4 ответа), менять только в случае, если в массиве присутствует достаточное количество ответов.
+    Public answers_a(10) As Integer ' Правильные ответы на вопросы. Количество правильных ответов равно количеству вопросов.
+    Public answer As Integer = 0 ' Переменная, хранящая номер ответа на вопрос, 0 - ответ не выбран, 1-4 - выбранный ответ. ЗНАЧЕНИЕ ВРУЧНУЮ НЕ ИЗМЕНЯТЬ.
+    Public i As Integer = 0 ' Счётчик для вопросов. ВРУЧНУЮ НЕ ИЗМЕНЯТЬ.
+    Public true_answers_count As Integer = 0 ' Счётчик правильных ответов. ВРУЧНУЮ НЕ ИЗМЕНЯТЬ.
+    Public Function config() ' Функция конфигурации, здесь вы можете ввести тестовый материал. В будущем конфигурация переедет в отдельный JSON файл.
+        questions = { ' Массив вопросов.
+        "Электронными таблицами являются следующие приложения:", ' Первый вопрос. Новая строка - новый вопрос.
+        "Прикладное программное обеспечение представляет собой:",
+        "К какому классу программного обеспечения принадлежат табличные редакторы?",
+        "Прикладные программы также называют:",
+        "К приложениям специального назначения относятся:",
+        "К приложениям общего назначения относятся:",
+        "К приложениям общего назначения относятся:",
+        "К приложениям специального назначения относятся:",
+        "Какие из приведенных названий приложений относятся к классу прикладного ПО общего назначения:",
+        "Какое из перечисленных названий обозначает класс прикладного программного обеспечения:"} ' Десятый вопрос, конец массива.
+        answers = { ' Массив ответов.
+            "Word", "Excel", "Access", "Powerpoint", ' Ответы на первый вопрос. Новая строка - новая четвёрка ответов.
+            "программы для защиты от компьютерных вирусов", "программы для обеспечения работы периферийного устройства", "программы для решения пользовательских задач обработки информации различного вида", "программы для обслуживания дискового пространства",
+            "Системного программного обеспечения", "Прикладного программного обеспечения общего назначения", "Интегрированных сред разработки приложений", "Специализированного прикладного программного обеспечению",
+            "драйверами", "утилитами", "браузерами", "приложениями",
+            "Программы для создания баз данных", "Интегрированные среды разработки приложений", "Экспертные системы", "Программы очистки дискового пространства",
+            "Программы для создания графических файлов", "Программы для подготовки печатной продукции", "Медицинские экспертные системы", "Программы для ведения бухгалтерского учета",
+            "Сервисные программы", "Программы для разработки пользовательских приложений", "Редакторы создания презентаций", "Геоинформационные системы",
+            "Программы для выполнения вычислений в электронных таблицах", "Программы для автоматизированного проектирования", "Операционные системы", "Программы для разработки других программ",
+            "MathCad", "Microsoft Access", "AutoCad", "1С бухгалтерия",
+            "специализированное", "сервисное", "системное", "стандартизированное"} ' Ответы на десятый вопрос, конец массива.
+        answers_a = {2, 3, 2, 4, 3, 1, 3, 2, 2, 1} ' Номера правильных ответов. Номер позиции ответа соответствует номеру вопроса.
+        Return 0 ' Возвращаем код успешного завершения выполнения функции. Это надо, чтобы IDE не ругалась.
+    End Function
+
+    Public Function next_question()
+        If i < (questions_count * 4) Then
+            ProgressBar1.Value = i / 4 + 1
+            Label2.Text = "Вопрос " + (i / 4 + 1).ToString + " из " + questions_count.ToString
+            Label3.Text = questions(i / 4)
+            RadioButton1.Checked = False
+            RadioButton2.Checked = False
+            RadioButton3.Checked = False
+            RadioButton4.Checked = False
+            RadioButton1.Text = answers(i)
+            RadioButton2.Text = answers(i + 1)
+            RadioButton3.Text = answers(i + 2)
+            RadioButton4.Text = answers(i + 3)
+        End If
+        Return 0 ' Возвращаем код успешного завершения выполнения функции. Это надо, чтобы IDE не ругалась.
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load ' Основная функция загрузки формы, срабатывает при запуске приложения. Это всё, что надо знать.
+        config()
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+        Label3.Text = questions(0)
+        RadioButton1.Text = answers(0)
+        RadioButton2.Text = answers(1)
+        RadioButton3.Text = answers(2)
+        RadioButton4.Text = answers(3)
+        ProgressBar1.Value = 1
+        Label2.Text = "Вопрос 1 из " + questions_count.ToString
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click ' кнопка "Проверить".
+        If i < (questions_count * 4) Then
+            If answer = answers_a(i / 4) Then
+                true_answers_count += 1
+            End If
+        End If
+
+        answer = 0
+        i += 4
+        next_question()
+        If i >= (questions_count * 4) Then
+            Label1.Text = "Ваш результат:"
+            Label3.Text = true_answers_count.ToString + " из " + questions_count.ToString + " (" + ((true_answers_count / questions_count) * 100).ToString + "%)" ' Выводим результат тестирования(даже с процентами).
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged ' Первый ответ
+        answer = 1
+    End Sub
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged ' Второй ответ
+        answer = 2
+    End Sub
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged ' Третий ответ
+        answer = 3
+    End Sub
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged ' Четвёртый ответ
+        answer = 4
+    End Sub
+End Class
